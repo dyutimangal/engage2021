c988dc224d884f2eb45aa565fc412d7043f8bec4
dyutimangal <dyutimangal@gmail.com>
2021-07-13 13:04:41 +0000

    comments2

--- server.js
+++ server.js
@@ -12,29 +12,42 @@ const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
 	debug: true,
 });
+
+//to store names of users
 const users = {};
 
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
 
+
+//root route will redirect to a room id
 app.get("/", (req, res) => {
 	res.redirect(`/${uuidv4()}`);
 });
 
+
+//the view "room" is rendered
 app.get("/:room", (req, res) => {
 	res.render("room", { roomId: req.params.room });
 });
 
+
 io.on("connection", (socket) => {
 	socket.on("join-room", (roomId, userId, userName) => {
 		socket.join(roomId);
+		
+		//used socket.id as unique ids to store names of users
 		users[socket.id] = userName;
+		
+		//emit msg 'user-connected' to start self video streaming on script side
 		io.to(roomId).emit('user-connected',userId, userName);
 		
+		//emit "creatMessage" when "message" received from script side
 		socket.on("message", (message) => {
 			io.to(roomId).emit("createMessage", message, userName);
 		});
 		
+		//emit 'user-disconnected' when a user disconnects and delete record
 		socket.on('disconnect', () => {
 			io.to(roomId).emit('user-disconnected', userId, userName);
 			delete users[socket.id];
